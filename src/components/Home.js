5c4f977ca944b3c5decc571ed936736555806066
cam <cameron.n.wright@gmail.com>
2020-11-01 10:00:15 +0200

    product elements  with some styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import homeSplash from "../images/home_splash.jpg";
+import productImageTemplate from "../images/product_image_template.jpg";
 import "../css/Home.css";
 import Product from "./Product.js";
 
@@ -8,11 +9,16 @@ function Home() {
     <div className="home">
       <div className="home__splashContainer">
         <img src={homeSplash} alt="MIN" />
-        <div className="home__row">
-          <Product />
-          <Product />
-          <Product />
-        </div>
+      </div>
+      <div className="home__row">
+        <Product
+          d={1}
+          image={productImageTemplate}
+          price={200}
+          title={"Speaker"}
+        />
+        <Product d={2} image={productImageTemplate} />
+        <Product d={3} image={productImageTemplate} />
       </div>
     </div>
   );
--- src/components/Product.js
+++ src/components/Product.js
@@ -4,8 +4,15 @@ import "../css/Product.css";
 function Product({ id, title, image, price }) {
   return (
     <div className="product">
-      <img src={""} alt="Product Image" />
-      <div className="product__title"></div>
+      <img
+        className="product__image product__image--fade"
+        src={image}
+        alt="Product Image"
+      />
+      <div className="product__infoContainer">
+        <h3>{title}</h3>
+        <span>{price}</span>
+      </div>
     </div>
   );
 }
